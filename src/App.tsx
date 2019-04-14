17a6b7b6e2932750a55d4a9822dbb224584d4fa5
sagirk <sagir.khan@gmail.com>
2019-04-14 13:18:17 +0530

    feat: Convert App to a functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 
-import Flexi from './components/Flexi';
-
-import { normalFlexiConfig, recursiveFlexiConfig } from './flexiConfig';
 import './App.css';
+import Flexi from './components/Flexi';
+import { normalFlexiConfig, recursiveFlexiConfig } from './flexiConfig';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Flexi Component Demo</h1>
-        </header>
-        <Flexi onSubmit={() => {}} config={recursiveFlexiConfig} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Flexi Component Demo</h1>
+      </header>
+      <Flexi onSubmit={() => {}} config={recursiveFlexiConfig} />
+    </div>
+  );
+};
 
 export default App;
