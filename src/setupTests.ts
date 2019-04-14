74760c8a8acdb496e2d1f3bf243fdd88dce2af9a
sagirk <sagir.khan@gmail.com>
2019-04-14 12:09:12 +0530

    feat: Add global setup for react-testing-library

---
+++ src/setupTests.ts
@@ -0,0 +1,5 @@
+// Automatically unmount and cleanup DOM after each test is finished.
+import 'react-testing-library/cleanup-after-each';
+
+// Add `jest-dom`'s custom assertions.
+import 'jest-dom/extend-expect';
